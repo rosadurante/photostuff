a87f3eb0755b7040142d12c98d8c5dd827a36392
Rosa Durante <me@rosadurante.com>
2014-11-16 18:23:27 +0000

    Added service controllers and states to photo module.

--- app/js/photo.js
+++ app/js/photo.js
@@ -1,7 +1,7 @@
 /* globals angular */
 
 /**
- * list.js
+ * photo.js
  *
  * Author: Rosa Durante <me@rosadurante.com>
  * Date: Sat 15th Nov 2014
@@ -10,29 +10,101 @@
 'use strict';
 
 var photo = {
-	config: function ($locationProvider, $stateProvider) {
-		$stateProvider
-			.state('photo', {
-				abstract: true,
-				url: '/photo',
-				templateUrl: '/partials/photo.html'
-			})
-			.state('photo.list', {
-				url: '',
-				templateUrl: '/partials/list.html',
-				resolve: {
-					photo: function () { return [1, 2, 3, 4, 5, 6]; }
-				}
-			})
-			.state('photo.detail', {
-				url: '/:photoId',
-				templateUrl: '/partials/detail.html',
-				controller: ['$scope', '$stateParams', function ($scope, $stateParams) {
-					$scope.photoId = $stateParams.photoId;
-				}]
-			});
-	}
+
+	// Dummy data
+	data: [{
+		id: 1,
+		title: 'cats!',
+		imgsrc: 'http://stylonica.com/wp-content/uploads/2014/03/cats-16140154-1920-1080.jpg',
+		date: 'Nov 23rd 2013',
+		link: 'http://google.co.uk',
+		description: 'description',
+		author: {
+			link: 'http://google.co.uk',
+			name: 'Perico el de los palotes'
+		},
+		tags: ['tag 1', 'tag 2', 'tag 3']
+	}, {
+		id: 2,
+		title: 'dogs!',
+		imgsrc: 'http://www.metrodogstop.com/cms/wp-content/uploads/2013/05/cute-dog.jpg',
+		date: 'Nov 24th 2013',
+		link: 'http://google.co.uk',
+		description: 'description',
+		author: {
+			link: 'http://google.co.uk',
+			name: 'Maria la hierbabuena'
+		},
+		tags: ['tag 3', 'tag 2', 'tag 1']
+	}]
 };
 
 angular.module('photo', ['ui.router'])
-	.config(['$locationProvider', '$stateProvider', photo.config]);
+	.config(['$locationProvider', '$stateProvider',
+
+		function ($locationProvider, $stateProvider) {
+
+			// Define all different urls within photo module.
+			// It has an abstract state which would act as root of this
+			// part of the web. Then, it defines a list and details views.
+
+			// These states will inject services and controllers
+			// defined below to resolve data and display it properly.
+
+			$stateProvider
+				.state('photo', {
+					abstract: true,
+					url: '/photo',
+					templateUrl: '/partials/photo.html'
+				})
+				.state('photo.list', {
+					url: '',
+					templateUrl: '/partials/list.html',
+					resolve: {
+						list: ['photoService', function (photoService) {
+							return photoService.getList();
+						}]
+					},
+					controller: 'photoListCtrl'
+				})
+				.state('photo.detail', {
+					url: '/:photoId',
+					templateUrl: '/partials/detail.html',
+					resolve: {
+						photo: ['$stateParams', 'photoService', function ($stateParams, photoService) {
+							return photoService.getById($stateParams.photoId);
+						}]
+					},
+					controller: 'photoDetailCtrl'
+				});
+		}
+	])
+
+	.factory('photoService', function () {
+
+		// Modules which will get data from resource provided (now, dummy data)
+		// and define a manager to provide those data nicely.
+
+		var _getList = function () { return photo.data; },
+			_getById = function (id) {
+				var selected;
+				angular.forEach(photo.data, function (item) {
+					if (!selected && item.id.toString() === id) { selected = item; }
+				});
+
+				return selected;
+			};
+
+		return {
+			getList: _getList,
+			getById: _getById
+		};
+	})
+
+	.controller('photoListCtrl', ['$scope', 'list',
+		function ($scope, list) { $scope.list = list; }
+	])
+
+	.controller('photoDetailCtrl', ['$scope', 'photo',
+		function ($scope, photo) { $scope.photo = photo; }
+	]);
