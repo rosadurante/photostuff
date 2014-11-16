3b9482564c620ea9384280703623d2d2392c752c
Rosa Durante <me@rosadurante.com>
2014-11-16 18:22:36 +0000

    Added config function within definition in angular. Added contact state

--- app/js/app.js
+++ app/js/app.js
@@ -9,24 +9,31 @@
 
 'use strict';
 
-var app = {
-	config: function ($locationProvider, $stateProvider) {
-		$locationProvider.html5Mode(true);
-
-		// $urlRouterProvider.otherwise('/');
-
-		$stateProvider
-			.state('home', {
-				url: '/',
-				templateUrl: '/partials/home.html'
-			})
-			.state('about', {
-				url: '/about',
-				templateUrl: '/partials/about.html'
-			});
-
-	}
-};
-
 angular.module('photostuff', ['ui.router', 'photo'])
-	.config(['$locationProvider', '$stateProvider', '$urlRouterProvider', app.config]);
+	.config(['$locationProvider', '$stateProvider', '$urlRouterProvider',
+
+		function ($locationProvider, $stateProvider, $urlRouterProvider) {
+
+			// Enable HTML5 mode
+			$locationProvider.html5Mode(true);
+			
+			// Redirect to home when page doesn't exists.
+			$urlRouterProvider.otherwise('/');
+
+			// Define top root urls for this app: home, about page and contact page.
+			$stateProvider
+				.state('home', {
+					url: '/',
+					templateUrl: '/partials/home.html'
+				})
+				.state('about', {
+					url: '/about',
+					templateUrl: '/partials/about.html'
+				})
+				.state('contact', {
+					url: '/contact',
+					templateUrl: '/partials/contact.html'
+				});
+		}
+
+	]);
