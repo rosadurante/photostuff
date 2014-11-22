bfddf834a1bf26daead1abd472f2e7e6ec097802
Rosa Durante <me@rosadurante.com>
2014-11-22 23:44:40 +0000

    Added filter to display HTML description from flickr

---
+++ app/js/photo/filter.js
@@ -0,0 +1,18 @@
+/* globals angular */
+
+/**
+ * photo.js
+ *
+ * Author: Rosa Durante <me@rosadurante.com>
+ * Date: Sat 15th Nov 2014
+ */
+
+'use strict';
+
+angular.module('photo')
+
+	.filter('rawHTML', ['$sce', function ($sce) {
+		return function (val) {
+			return $sce.trustAsHtml(val);
+		};
+	}]);
